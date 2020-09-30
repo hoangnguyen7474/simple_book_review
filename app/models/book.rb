8285bbd09f99df867a49be610a07669aa9582d59
Hoang Nguyen <hoangnguyen7492@gmail.com>
2020-09-30 16:51:37 +0700

    Moving away from Paperclip gem

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
   belongs_to :user
   resourcify
   has_many :comments, dependent: :destroy
-  has_attached_file :image, styles: { large: "500x500", medium: "300x300>", thumb: "150x150>" },  default_url: "/images/No_cover_available.jpg"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  has_one_attached :image, dependent: :destroy
+  validates :image, content_type: [:png, :jpg, :jpeg]
+ 
 end
