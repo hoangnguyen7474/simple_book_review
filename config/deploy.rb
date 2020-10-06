0217dab62d8e2b362bb858b6af2426f5aabeac86
Hoang Nguyen <hoangnguyen7492@gmail.com>
2020-10-06 19:33:32 +0700

    ec2

---
+++ config/deploy.rb
@@ -0,0 +1,30 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.14.1"
+
+require 'capistrano-db-tasks'
+
+set :application, 'simple_book_review'
+set :repo_url, 'https://github.com/hoangnguyen7474/simple_book_review'
+set :deploy_to, 'home/deploy/simple_book_review'
+set :branch, ENV['BRANCH'] if ENV['BRANCH']
+
+set :linked_files, %w{config/database.yml config/master.key}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+set :keep_releases, 3
+set :keep_assets, 3
+
+set :db_local_clean, true
+set :db_remote_clean, true
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end  
+  end
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+end
