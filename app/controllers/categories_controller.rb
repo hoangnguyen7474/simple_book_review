ce5a0b696d125a355964b0fae76633b23be89e53
Hoang Nguyen <hoangnguyen7492@gmail.com>
2020-10-08 16:15:17 +0700

    fix review section

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
   load_and_authorize_resource
 
   def index    
-    @categories = Category.all
+    @categories = Category.category_all
   end
 
   def show
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   has_many :books 
   resourcify
+
+  scope :category_all, -> { order(category_name: :asc) }
 end
