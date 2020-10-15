080b495fe7d0d70a479890d01ea2545d9f5a2703
Hoang Nguyen <hoangnguyen7492@gmail.com>
2020-10-15 16:45:22 +0700

    refactor Book Model and Book Controller

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -52,8 +52,8 @@ namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/demodeploy`
-        puts "WARNING: HEAD is not the same as origin/demodeploy"
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
         puts "Run `git push` to sync changes."
         exit
       end
